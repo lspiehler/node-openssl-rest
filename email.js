655f7bd9eb04ca01f2a8f1be38ab3840f3728248
Lyas Spiehler <lspiehler@gmail.com>
2019-05-22 21:36:11 -0500

    enabled the ability to set email params using environment variables

--- email.js
+++ email.js
@@ -1,22 +1,23 @@
 var email = {
-	
-	//email address to send usage data
-	emailParams: false,
-	
-	nodemailertransportparams: {
-		host: 'notjustnetworks.com',
-		port: 465,
-		secure: true
-	},
-	
-	nodemailermailoptions: {
-		from: '"Node OpenSSL Rest" <notifications@notjustnetworks.com>', // sender address
-        to: 'notifications@notjustnetworks.com', // list of receivers
-        subject: 'node-openssl-rest data', // Subject line
-        //text: 'Hello world?', // plain text body
-        //html: '<b>Hello world?</b>' // html body
-	}
-	
+
+        //email address to send usage data
+        emailParams: process.env.EMAILPARAMS || true,
+
+        nodemailertransportparams: {
+                host: process.env.EMAILHOST || 'notjustnetworks.com',
+                port: process.env.EMAILPORT || 465,
+                secure: process.env.EMAILSECURE || true
+        },
+
+        nodemailermailoptions: {
+                from: process.env.EMAILFROM || '"Node OpenSSL Rest" <notifications@notjustnetworks.com>', // sender address
+                to: process.env.EMAILTO || 'notifications@notjustnetworks.com', // list of receivers
+                subject: process.env.EMAILSUBJECT || 'node-openssl-rest data', // Subject line
+                auth: {
+                        user: process.env.EMAILUSER || 'notifications', // generated ethereal user
+                        pass: process.env.EMAILPASS || 'Ls67593176043!' // generated ethereal password
+                }
+        }
 }
 
 module.exports = email;
